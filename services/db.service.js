3f28f42466529f373a1fa741c3679ef76c35354f
Max Choheli <max.choheli@gmail.com>
2025-09-16 22:57:51 +0300

    fixing the code 500

--- services/db.service.js
+++ services/db.service.js
@@ -1,34 +1,35 @@
 import { MongoClient } from 'mongodb'
+import { logger } from './logger.service.js'
 
-import { config } from '../config/index.js'
+let client
+let db
 
 export const dbService = {
 	getCollection,
 }
 
-var dbConn = null
-
-async function getCollection(collectionName) {
+async function connect() {
+	if (db) return db
+	const uri = process.env.MONGO_URL
+	if (!uri) throw new Error('Missing MONGO_URL')
 	try {
-		const db = await _connect()
-		const collection = await db.collection(collectionName)
-		return collection
+		client = new MongoClient(uri)
+		await client.connect()
+		db = client.db()
+		logger.info('Connected to MongoDB')
+		return db
 	} catch (err) {
-		logger.error('Failed to get Mongo collection', err)
+		logger.error('Cannot connect to DB', err)
 		throw err
 	}
 }
 
-async function _connect() {
-	if (dbConn) return dbConn
+async function getCollection(collectionName) {
 	try {
-		// const client = await MongoClient.connect(config.dbURL, { useUnifiedTopology: true })
-		const client = await MongoClient.connect(config.dbURL)
-		const db = client.db(config.dbName)
-		dbConn = db
-		return db
+		const database = await connect()
+		return database.collection(collectionName)
 	} catch (err) {
-		logger.error('Cannot Connect to DB', err)
+		logger.error('Failed to get Mongo collection', err)
 		throw err
 	}
 }
